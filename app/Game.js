05850c90531ea8efccfd2479ca5e6a7e17b37480
pemix09 <pemix09@onet.pl>
2021-11-20 11:38:28 +0100

    Game is done!

--- app/Game.js
+++ app/Game.js
@@ -2,6 +2,8 @@ import Quote from './Quote.js';
 import { GetQuotes } from './GetQuotes.js';
 
 export default class Game {
+  currentStep = 0;
+  lastStep = 7;
   quotes = GetQuotes();
 
   constructor({ lettersWrapper, categoryWrapper, wordWrapper, outputWrapper }) {
@@ -16,25 +18,46 @@ export default class Game {
     this.quote = new Quote(quote);
   }
 
-  guess(letter) {
-    this.quote.guess(letter);
-    this.drawQuote();
+  guess(letter,event) {
+    event.target.disabled = true;
+    if(this.quote.guess(letter)){
+      this.drawQuote();
+    }
+    else{
+      this.currentStep++;
+      document.getElementsByClassName('step')[this.currentStep].style.opacity = 1;
+      if(this.currentStep == this.lastStep){
+        this.loose();
+      }
+    }
   }
 
+  win(){
+    this.word.innerHTML = 'Gratulacje, wygrałeś!';
+    this.letters.innerHTML = '';
+  }
+  loose(){
+    this.word.innerHTML = 'Przegrywasz!';
+    this.letters.innerHTML = '';
+  }
   drawLetters() {
     for (let i = 0; i < 26; i++) {
       const label = (i + 10).toString(36);
       const button = document.createElement("button");
       button.innerHTML = label;
-      button.addEventListener("click", () => this.guess(label));
+      button.addEventListener("click", (event) => this.guess(label,event));
       this.letters.appendChild(button);
     }
   }
   drawQuote(){
     this.content = this.quote.getContent();
     this.word.innerHTML = this.content;
+    if(!this.content.includes('_')){
+      this.win();
+    }
   }
   start() {
+    document.getElementsByClassName('step')[this.currentStep].style.opacity = 1;
     this.drawLetters();
     this.drawQuote();  
   }
