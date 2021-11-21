6087f21985707de782c96795d8013cda27a53e0b
pemix09 <pemix09@onet.pl>
2021-11-21 12:28:39 +0100

    Added more quotes

--- app/GetQuotes.js
+++ app/GetQuotes.js
@@ -8,6 +8,24 @@ export function GetQuotes()
         {
             quote: 'juliusz slowacki',
             category: 'Sławne osoby'
-        }];
+        },{
+            quote: 'donald trump',
+            category: 'Sławne osoby'
+        },{
+            quote: 'scooby doo',
+            category: 'Sławne postacie'
+        },{
+            quote: 'krolowa elzbieta',
+            category: 'Sławne osoby'
+        },{
+            quote: 'aston martin',
+            category: 'samochody'
+        },{
+            quote: 'komputer',
+            category: 'rzeczy'
+        },{
+            quote: 'telefon komorkowy',
+            category: 'rzeczy'
+        },];
     return quotes;
 }
